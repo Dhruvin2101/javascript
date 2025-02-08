53fd90bc3bc59c8fce8f6f66003ae14154d001e3
Dhruvin Patel <149464919+Dhruvin2101@users.noreply.github.com>
2025-02-08 19:34:27 +0530

    Changes

--- 01_Basics/01_variable.js
+++ 01_Basics/01_variable.js
@@ -1,6 +1,6 @@
 const emailName = "abc@gmail.com";
 var fullName = "abc";
 let lastName = "bcd";
+let email ;
 
-console.table([emailName, fullName, lastName]);
-console.log("Hello World");
+console.table([emailName, fullName, lastName, email]);
---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // for newer version of js//
+
+// alert("hello"); using node js not browser
+
+/* number
+    bigint
+    string ""
+    boolean => true / false
+    null => represents empty value
+    undefined => the value is not assigned yet
+    symbol => unique
+    object
+*/
+
+console.log(typeof undefined) // returns undefined
+console.log(typeof null) // returns object
