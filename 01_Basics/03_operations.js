e3c5af6055697971b9b58370b4362b801cfb85d6
Dhruvin Patel <149464919+Dhruvin2101@users.noreply.github.com>
2025-03-07 00:51:57 +0530

    conversion of datatypes

--- 01_Basics/03_operations.js
+++ 01_Basics/03_operations.js
@@ -1,6 +1,52 @@
 // Hoisting => It basically means that the JS reads the code from top to bottom format so if we try to print the thing which is initialized later
 
-console.log(a())
-function a(){
-    console.log("hello")
-}
+//Conversion
+
+let score = "33abc";
+
+console.log(typeof(score));
+
+let new_score = Number(score);
+console.log(typeof(new_score));
+console.log(new_score);
+
+console.log("--------------------------------");
+
+let hello = null;
+
+console.log(typeof(hello));
+
+let new_hello = Number(hello);
+console.log(typeof(new_hello));
+console.log(new_hello);
+
+console.log("--------------------------------");
+
+let bus = undefined;
+
+console.log(typeof(bus));
+
+let new_bus = Number(bus);
+console.log(typeof(new_bus));
+console.log(new_bus);
+
+/* "33" => 33 
+    "33abc" => NaN
+    null => 0
+    true => 1; false => 0
+*/
+console.log("--------------------------------");
+
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
+
+/* 1 => true
+    "" => false
+    "dhruvin" => true
+*/
+console.log("--------------------------------");
+
+
