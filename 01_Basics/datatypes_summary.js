6f74c46c6e01aa6bdc22361374e823df2576ebfa
Dhruvin Patel <149464919+Dhruvin2101@users.noreply.github.com>
2025-03-07 09:55:15 +0530

    Datatypes

---
+++ 01_Basics/datatypes_summary.js
@@ -0,0 +1,10 @@
+// stack (primitive) , Heap (Non-Primitive)  
+
+let stringName = "    hello@gmail.com    ";
+
+console.log(stringName.length);
+console.log(stringName.substring(0 , 8)); //returns value from start to end
+console.log(stringName.trim());
+console.log(stringName.split("@"));
+console.log(stringName.toLowerCase());
+console.log(stringName.toUpperCase());
